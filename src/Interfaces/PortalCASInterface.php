d1ce93154606f9139c9c218f6ef91653c62fb9c7
Megoc <megoc@megoc.org>
2019-01-19 17:28:27 +0800

    添加接口

--- src/Interfaces/PortalCASInterface.php
+++ src/Interfaces/PortalCASInterface.php
@@ -11,5 +11,33 @@ namespace Megoc\Ecjtu\Interfaces;
 
 interface PortalCASInterface
 {
+    /**
+     * cas authority service
+     *
+     * @param string $service_cas_uri
+     * @return string
+     */
+    public function cas_authority_link(string $service_cas_uri = '');
+    /**
+     * login system
+     *
+     * @param array $user
+     * @return void
+     */
+    public function login(array $user = []);
+    /**
+     * encrpt password
+     *
+     * @param string $password
+     * @return string
+     */
+    public function encrypted_password($password = '');
+    /**
+     * servie name to servie cas server url 
+     *
+     * @param string $service_name
+     * @return string
+     */
+    public function service_name2service_uri($service_name = '');
 
 }
